d9fe6d6236f41a16a7f488330f9c5ecd5bbbc9c8
gloria campos <gloriacampos@glorias-MacBook-Pro.local>
2018-05-22 16:29:55 -0700

    Intro to Console Log

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-var myName = "Gloria";
+var myHobby = "Some of my favorite hobbies include: chillin' hard on the couch, eating tapatio doritos, anything active and trying different fruits. I hope to have a better understanding of how the digital world works by the end of this course.";
 
-console.log(myName);
+console.log(myHobby);
