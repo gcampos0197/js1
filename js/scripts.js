effe42938238732c905ea6ef5c659f57ffd68b13
gloria campos <gloriacampos@glorias-MacBook-Pro.local>
2018-05-23 21:21:03 -0700

    Program including Popmts Alerts Consol Log

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
+/*
 var myHobby = "Some of my favorite hobbies include: chillin' hard on the couch, eating tapatio doritos, anything active and trying different fruits. I hope to have a better understanding of how the digital world works by the end of this course.";
 
 console.log(myHobby);
+*/
+
+/*1. can consolelog
+1. Console.log(name, age, goal); no spaces
+2. Console.log(name + age + goal); spaces
+*/
+
+/*let favFood = 'turkey club';
+let favMovie = 'Inglorious Bastards';
+const year = new Date(6/23/2010);
+const ingredients = ["crossiant", "turkey", "tomato", "avacado", "lettuce"];
+
+alert(favFood + ingredients);
+confirm(favMovie + year);*/
+
+
+
+let userFirstName = prompt('What is your first name?')
+let userLastName = prompt('What is your last name?')
+let birthday = prompt('When is your birthday?')
+
+console.log(userFirstName)
+
+alert(userLastName)
+alert(birthday)
